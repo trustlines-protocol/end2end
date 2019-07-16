38d9b2df8ee975a40d55c3a5ff6e753850b8105c
Ralf Schmitt <ralf@brainbot.com>
2019-07-16 14:44:42 +0200

    Enable shfmt pre-commit hook
    Also reformat run-e2e.sh with shfmt.

--- run-e2e.sh
+++ run-e2e.sh
@@ -4,13 +4,12 @@ NYC_OUTPUT_DIR=.nyc_output
 
 set -u
 
-function die()
-{
-    echo $1
-    exit 1
+function die() {
+  echo $1
+  exit 1
 }
 
-OPTIND=1         # Reset in case getopts has been used previously in the shell.
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
 cwd=$(pwd)
 # Initialize our own variables:
 use_local_yarn=0
@@ -18,41 +17,41 @@ pull=0
 coverage=0
 only_backend=0
 while getopts "lpcb" opt; do
-    case "$opt" in
-        l)  use_local_yarn=1
-            test -e src/Trustline.ts || die "run-e2e.sh: local test runs must be started from the clientlib repository"
-            ;;
-        p)  pull=1
-            ;;
-        c)  coverage=1
-            ;;
-        b)  only_backend=1
-            ;;
-    esac
+  case "$opt" in
+  l)
+    use_local_yarn=1
+    test -e src/Trustline.ts || die "run-e2e.sh: local test runs must be started from the clientlib repository"
+    ;;
+  p)
+    pull=1
+    ;;
+  c)
+    coverage=1
+    ;;
+  b)
+    only_backend=1
+    ;;
+  esac
 done
-
 
 # Makes the bash script to print out every command before it is executed except echo
 # this is a replacement for 'set -x'
-function preexec ()
-{
-    [[ $BASH_COMMAND != echo* ]] && echo >&2 "+ $BASH_COMMAND"
+function preexec() {
+  [[ $BASH_COMMAND != echo* ]] && echo >&2 "+ $BASH_COMMAND"
 }
-set -o functrace   # run DEBUG trap in subshells
+set -o functrace # run DEBUG trap in subshells
 trap preexec DEBUG
 
-
-function cleanup()
-{
-    cd $E2E_DIR
-    docker-compose down -v
-    rm -r $mydir
+function cleanup() {
+  cd $E2E_DIR
+  docker-compose down -v
+  rm -r $mydir
 }
 
 E2E_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 cd $E2E_DIR
 if test $pull -eq 1; then
-    docker-compose pull
+  docker-compose pull
 fi
 
 mydir=$(mktemp -td end2end.XXXXXX)
@@ -75,41 +74,41 @@ docker-compose up init
 docker-compose up -d index relay
 
 if test $only_backend -eq 0; then
-    sleep 3
-    docker-compose logs -t -f parity index relay e2e &
-    if test $use_local_yarn -eq 0; then
-        docker-compose up -d e2e
-        docker_wait_output=$(docker wait e2e)
-        case $docker_wait_output in
-            [0-9]*)
-                result=$docker_wait_output
-                ;;
-            *)
-                result=1
-                ;;
-        esac
-        docker-compose logs -t e2e >$mydir/output.txt
-        cd $cwd
-        if test $coverage -eq 1; then
-            rm -rf "$NYC_OUTPUT_DIR"
-            docker cp e2e:/clientlib/"$NYC_OUTPUT_DIR"/. "$NYC_OUTPUT_DIR"
-        fi
-    else
-        cd $cwd
-        yarn run test:e2e | tee $mydir/output.txt
-        result="${PIPESTATUS[0]}"
+  sleep 3
+  docker-compose logs -t -f parity index relay e2e &
+  if test $use_local_yarn -eq 0; then
+    docker-compose up -d e2e
+    docker_wait_output=$(docker wait e2e)
+    case $docker_wait_output in
+    [0-9]*)
+      result=$docker_wait_output
+      ;;
+    *)
+      result=1
+      ;;
+    esac
+    docker-compose logs -t e2e >$mydir/output.txt
+    cd $cwd
+    if test $coverage -eq 1; then
+      rm -rf "$NYC_OUTPUT_DIR"
+      docker cp e2e:/clientlib/"$NYC_OUTPUT_DIR"/. "$NYC_OUTPUT_DIR"
     fi
-    cat $mydir/output.txt
-    exit $result
+  else
+    cd $cwd
+    yarn run test:e2e | tee $mydir/output.txt
+    result="${PIPESTATUS[0]}"
+  fi
+  cat $mydir/output.txt
+  exit $result
 else
-    echo
-    echo "====================================================="
-    echo "Initialization is complete."
-    echo "You've started the script with the -b flag."
-    echo "You can now run your tests manually."
-    echo "Hit Ctrl-C when done."
-    echo "====================================================="
-    echo
-    sleep 5
-    docker-compose logs -t -f parity index relay
+  echo
+  echo "====================================================="
+  echo "Initialization is complete."
+  echo "You've started the script with the -b flag."
+  echo "You can now run your tests manually."
+  echo "Hit Ctrl-C when done."
+  echo "====================================================="
+  echo
+  sleep 5
+  docker-compose logs -t -f parity index relay
 fi
